d23a71684b8180dd770706f69fabd65007d3f211
AmirthavalliG <amirthavalli.govindarajan@sap.com>
2023-11-02 22:35:26 +0530

    refactor: remove unnecessary modules
    cleaned up dependency from flp, added only the sample app

--- mission-patient-list/apps/patientlistwithapplyrules/webapp/controller/App.controller.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define([ "sap/ui/core/mvc/Controller" ], function(Controller) {
-	"use strict";
-
-	return Controller.extend("com.sap.health.example.mission.patientlist.controller.App", {
-		onInit : function() {
-			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
-		}
-	});
-});
